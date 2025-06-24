3fdbdbb2577bd71de8f4d0f433b9145738a21942
Eliseo Quintanilla <eliseo.quintanillagz@gmail.com>
2025-06-24 10:38:32 +0200

    commit to change branch

--- src/06_objects/compareObjects.js
+++ src/06_objects/compareObjects.js
@@ -6,7 +6,28 @@
  * @param {*} obj2 
  */
 function compareObjects(obj1, obj2) {
-    // Your code here
+    if (typeof obj1 !== "object" || typeof obj2 !== "object" || obj1 === null || obj2 === null) return false;
+    
+    const obj1Keys = Object.keys(obj1);
+    const obj2Keys = Object.keys(obj2);
+
+    if (obj1Keys.length !== obj2Keys.length) return false;
+
+    for (let key of obj1Keys) {
+        if (!obj2Keys.includes(key)) {
+            return false;
+        }
+
+        if (typeof obj1[key] !== "object" || typeof obj2[key] !== "object") {
+            if (obj1[key] !== obj2[key]) {
+                return false;
+            } 
+        } else {
+            compareObjects(obj1[key], obj2[key])
+        }
+    }
+    
+    return true;
 }
   
 module.exports = compareObjects;
